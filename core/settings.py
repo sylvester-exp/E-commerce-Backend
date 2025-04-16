3c0dace2423731cf53b3ed748733c530aee9b001
Abigail sylvester <abigailj.sylvester@gmail.com>
2025-04-16 12:21:12 +0100

    Update CORS settings and push changes to GitHub

--- core/settings.py
+++ core/settings.py
@@ -52,9 +52,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -153,7 +153,8 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Allow frontend to access Django API
+    "http://localhost:5173",
+      "http://127.0.0.1:5173",  # Allow frontend to access Django API
 ]
 
 CORS_ALLOW_CREDENTIALS = True  # If using authentication cookies
